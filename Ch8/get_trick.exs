0cf8ea9280074f3bdf6081c5c9064f8293944cf8
Bogdan Costea <koboldul@gmail.com>
2018-12-08 10:56:19 -0500

    dd

--- Ch8/get_trick.exs
+++ Ch8/get_trick.exs
@@ -1,17 +1,16 @@
-# authors = [
-#   %{name: "Jose", language: "Elixir", m: %{a: {"C", "A"}, b: "B"}}
+authors = [
+  %{name: "Jose", language: "Elixir", m: %{a: {"C", "A"}, b: "B"}}
+]
 
-# ]
+r_select = fn (:get, collection, next_fn) ->
+  for row <-collection do
+    if String.contains?(row.language, "r") do
+      next_fn.(row)
+    end
+  end
+end
 
-# r_select = fn (:get, collection, next_fn) ->
-#   for row <-collection do
-#     if String.contains?(row.language, "r") do
-#       next_fn.(row)
-#     end
-#   end
-# end
-
-# IO.inspect get_in(authors, [r_select, :language])
+IO.inspect get_in(authors, [r_select, :language])
 wtf = %{name: "Jose", language: "Elixir"}
 IO.inspect get_in(wtf, [​:name])
 
